9fcef859b05a6cc851819a5941b713ba02441bce
Alex <alexander.hemming@gmail.com>
2023-12-04 15:52:07 +0000

    chore:(17): vercel eslint

--- src/pages/Second-Board/index.tsx
+++ src/pages/Second-Board/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { ColumnProps } from "../../components/Collumn/Collumn";
 import { CardProps } from "../../components/Card/Card";
-
 
 import Link from "next/link";
 
 function SecondBOard() {
   const value = useSelector((state: any) => state.feature1.value);
+  let store = [
+    {
+      id: "0",
+      status: "no collumns available",
+      cards: [{ id: 0, description: "no cards available", date: "n/a" }],
+    },
+  ];
+
+  try {
+    if (value) {
+      store = JSON.parse(value);
+    }
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+  }
 
   return (
     <div>
@@ -16,9 +29,7 @@ function SecondBOard() {
       </div>
 
       <div className="flex justify-center mx-auto border w-20 px-3 py-1 rounded hover:bg-gray-800 transition-all">
-        <Link className="" href="/">
-          Home
-        </Link>
+        <Link href="/">Home</Link>
       </div>
 
       <div className="flex justify-center">
@@ -27,11 +38,11 @@ function SecondBOard() {
       </div>
 
       <div className="flex justify-center mt-4">
-        {JSON.parse(value).map((data: ColumnProps) => (
+        {store?.map((data: any) => (
           <div key={data?.id} className="mx-4 ">
             {data.status}
             <br />
-            {data.cards?.map?.((input: CardProps) => (
+            {data?.cards?.map?.((input: CardProps) => (
               <div key={input?.id} className="my-2 ">
                 date: {input.date}
                 <br />
